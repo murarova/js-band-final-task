2991d433c75f58aeba24cce517cdf185597c2bea
Liubov Murarova <murarova.lubov@gmail.com>
2019-12-15 16:17:17 +0200

    Updates

---
+++ precache-manifest.4aed65e12ac27a7dfbf1ea1db6361ef5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7603af4b538b66eb9c41c53272d2bda3",
+    "url": "/js-band-final-task/index.html"
+  },
+  {
+    "revision": "108251df5caa64307bd3",
+    "url": "/js-band-final-task/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "aeec95f988cbd40c848a",
+    "url": "/js-band-final-task/static/css/main.3e7f67f2.chunk.css"
+  },
+  {
+    "revision": "108251df5caa64307bd3",
+    "url": "/js-band-final-task/static/js/2.833531db.chunk.js"
+  },
+  {
+    "revision": "aeec95f988cbd40c848a",
+    "url": "/js-band-final-task/static/js/main.f457adb2.chunk.js"
+  },
+  {
+    "revision": "49af006cc0cf04963d6b",
+    "url": "/js-band-final-task/static/js/runtime-main.3be8268d.js"
+  },
+  {
+    "revision": "a0c11df4e57857bdd663a77dea4f5023",
+    "url": "/js-band-final-task/static/media/logout.a0c11df4.svg"
+  },
+  {
+    "revision": "a6d923777b168748329fda0c74b055a1",
+    "url": "/js-band-final-task/static/media/shopping-cart.a6d92377.svg"
+  },
+  {
+    "revision": "d26054928b6d5dd777f60c2407261d7a",
+    "url": "/js-band-final-task/static/media/teamwork.d2605492.svg"
+  }
+]);
